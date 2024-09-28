beef14b6cc54e2afa9032a92d4ba6cf8bcaa0d00
VT <144271865+OppasVTzin@users.noreply.github.com>
2024-09-28 12:49:54 -0300

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,34 +1,34 @@
 export default [
     {
-      src: "./src/BIGBANG - 뱅뱅뱅 (BANG BANG BANG) MV.mp3",
+      src: "BIGBANG.mp3",
       name: "BIGBANG - 뱅뱅뱅 (BANG BANG BANG) MV.mp3",
     },
     {
-      src: "./src/BLACKPINK - Shut Down MV.mp3",
+      src: "BLACKPINK - Shut Down MV.mp3",
       name: "BLACKPINK - Shut Down MV",
     },
     {
-      src: "./src/BTS - Dope (LegendadoTradução).mp3",
+      src: "BTS - Dope (LegendadoTradução).mp3",
       name: "BTS - Dope (LegendadoTradução)",
     },
     {
-      src: "./src/MAMA.mp3",
+      src: "MAMA.mp3",
       name: "EXO-K - MAMA",
     },
     {
-      src: "./src/Stray Kids MANIAC MV.mp3",
+      src: "Stray Kids MANIAC MV.mp3",
       name: "Stray Kids - MANIAC MV",
     },
     {
-      src: "./src/EXO-K 엑소케이 History MV (Korean Ver.).mp3",
+      src: "History.mp3",
       name: "EXO-K 엑소케이 History MV (Korean Ver.)",
     },
     {
-        src: "./src/GOT7 Just right(딱 좋아) MV.mp3",
+        src: "Just Right.mp3",
         name: "GOT7 Just right(딱 좋아) MV",
     },
     {
-        src: "./src/SUPER JUNIOR 슈퍼주니어 Devil Performance Video.mp3",
+        src: "Devil.mp3",
         name: "SUPER JUNIOR 슈퍼주니어 Devil Performance Video",
     },
 
